4d3b63fe55dfd2fbcc64be32a0947d962268c6db
go <zhoushilian1995@outlook.com>
2018-05-29 20:02:22 +0800

    完善表结构

--- DB.md
+++ DB.md
@@ -12,7 +12,6 @@ password|varchar(20)|用户密码|×|-
 uk_email|varchar(30)|用户注册邮箱|×|-
 self_introduction|varchar(200)|自我介绍|√|-
 header|varchar(40)|用户头像|√|default.jpg
-tag_ids|varchar(40)|关注标签ID集合|√|"1"（默认关注Java,多个ID之间用-来连接）
 gmt_create|date_time|用户创建时间|×|-
 gmt_modified|date_time|用户修改时间|×|-|
 - - - -
@@ -69,3 +68,36 @@ post_id|unsigned int|关联帖子ID|×|-
 gmt_create|date_time|此关系创建时间|×|-
 gmt_modified|date_time|此关系修改时间|×|-
 - - - -
+:six: 标签和用户关系表（tag_user_rel）
+
+列名|数据类型|备注|能否为空|默认值
+:----:|:----:|:----:|:----:|:----:
+tu_id|unsigned int|主键|×|-
+tag_id|unsigned int|关联标签ID|×|-
+u_id|unsigned int|关联用户ID|×|-
+gmt_create|date_time|此关系创建时间|×|-
+gmt_modified|date_time|此关系修改时间|×|-
+
+- - - -
+:seven: 用户收藏表（col_post_rel）
+
+列名|数据类型|备注|能否为空|默认值
+:----:|:----:|:----:|:----:|:----:
+cp_id|unsigned int|主键|×|-
+u_id|unsigned int|关联用户ID|×|-
+p_id|unsigned int|关联帖子ID|×|-
+gmt_create|date_time|此关系创建时间|×|-
+gmt_modified|date_time|此关系修改时间|×|-
+
+- - - - 
+:eight: 用户关注表(user_follow)
+
+列名|数据类型|备注|能否为空|默认值
+:----:|:----:|:----:|:----:|:----:
+uf_id|unsigned int|主键|×|-
+u_id|unsigned int|关联用户ID|×|-
+follow_id|unsigned int|关联关注的用户ID|×|-
+gmt_create|date_time|此关系创建时间|×|-
+gmt_modified|date_time|此关系修改时间|×|-
+
+- - - - 
--- avalon/src/test/java/org/pino/dao/UserMapperTest.java
+++ avalon/src/test/java/org/pino/dao/UserMapperTest.java
@@ -3,6 +3,8 @@ package org.pino.dao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.pino.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,11 +19,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:spring/spring-*.xml")
 public class UserMapperTest {
 
+//    private static final Logger LOG = LoggerFactory.getLogger(UserMapperTest.class);
+
     @Autowired
     private UserMapper userMapper;
 
     @Test
-    public void insertUserTest(){
+    public void insertUserTest() {
         User user = new User();
         user.setUkUsername("jack");
         user.setUkEmail("2287861912@qq.com");
@@ -29,7 +33,21 @@ public class UserMapperTest {
         user.setPassword("123123");
         user.setTagIds("1-2");
         user.setSelfIntroduction("asdfs");
-
         userMapper.insertUser(user);
+    }
+
+    @Test
+    public void checkUsername() {
+        System.out.println(userMapper.checkUsername("aaa"));
+    }
+
+    @Test
+    public void updatePassword(){
+        userMapper.updatePassword("1","aaaaaa");
+    }
+
+    @Test
+    public void update(){
+        userMapper.updateSelfIntroduce("1","abcdefg");
     }
 }
